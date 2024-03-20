a303c1ee9a2f69dff76953c50897021d4efbaa13
logannii <loganzhang0416@gmail.com>
2024-03-20 11:24:29 +0000

    Add zblur functionality

--- ppr/ppr.py
+++ ppr/ppr.py
@@ -6,6 +6,7 @@ import pandas as pd
 import regtricks as rt
 import h5py
 import logging
+from scipy.ndimage import gaussian_filter, convolve1d
 import argparse
 from glob import glob
 
@@ -81,7 +82,33 @@ def _calc_norm_factor(oxdir, model_dict, fsdir=None):
     return norm_factor
 
 
-def predict(oxdir, modeldir, age, gender, space):
+def _gauss_kernel(sigma_mm, nifti):
+    """
+    1D Gaussian kernal in voxel units (converted from mm)
+ 
+    Args:
+        sigma_mm: FWHM of Gaussian kernel in mm
+        nifti: Nifti1Image object defining target voxel grid
+ 
+    Returns:
+        1D np.array of variable length
+    """
+ 
+    vox_size = nifti.header["pixdim"][1:4]
+    if vox_size[2] <= 2:
+        logging.warning("Voxel size in z-direction is small, z-blurring may not be appropriate")
+        dx = np.array([0, vox_size[2]])
+    else:
+        dx = np.arange(0, vox_size[2] * vox_size[2] // 2, vox_size[2])
+    fx = np.exp(-(dx**2) / (2 * sigma_mm**2))
+    fx = fx[fx > 0.1 * fx.max()]
+    fx = np.concatenate([fx[::-1], fx[1:]])
+    fx = fx / fx.sum()
+    assert fx.size % 2, "filter should have odd size"
+    return fx
+
+
+def predict(oxdir, modeldir, age, gender, space, outfolder=None, zblur=False):
     """Predicts the baseline perfusion image for a given subject. 
 
     Parameters
@@ -96,9 +123,16 @@ def predict(oxdir, modeldir, age, gender, space):
         The gender of the subject. Either 'M' or 'F'
     space : str, optional
         The space of the output image. Either 'native' 'struct' or 'std'.
+    outfolder : str, optional
+        The output folder name for the prediction output. If not provided, the output folder name will be the same as the model name.
+    zblur : bool, optional
+        Whether to apply a Gaussian blur along the z-axis to the output image.
     """
 
-    outdir = op.join(oxdir, "ppr", op.basename(modeldir).split(".")[0], space)
+    if outfolder is None:
+        outfolder = op.basename(modeldir).split(".")[0]
+
+    outdir = op.join(oxdir, "ppr", outfolder, space)
     os.makedirs(outdir, exist_ok=True)
     logging.basicConfig(
         filename=op.join(outdir, "ppr.log"),
@@ -263,16 +297,32 @@ def predict(oxdir, modeldir, age, gender, space):
 
     target_spc.save_image(prediction, op.join(outdir, "prediction.nii.gz"))
     logging.info(f"Predicted baseline perfusion image saved to {op.join(outdir, 'prediction.nii.gz')}")
+
+    if zblur:
+        logging.debug("Applying Gaussian blur along z-axis")
+        kernel = _gauss_kernel(5, nib.load(op.join(outdir, "prediction.nii.gz")))
+        logging.debug(f"Kernel: {kernel}")
+        prediction_zblur = convolve1d(prediction, kernel, axis=2)
+        target_spc.save_image(prediction_zblur, op.join(outdir, "prediction_zblur.nii.gz"))
+        logging.info(f"Predicted baseline perfusion image with z-axis blurring saved to {op.join(outdir, 'prediction_zblur.nii.gz')}")
+
     target_spc.save_image(truth, op.join(outdir, "truth.nii.gz"))
     logging.info(f"Ground truth perfusion image saved to {op.join(outdir, 'truth.nii.gz')}")
     difference = truth - prediction
     difference[prediction == 0] = 0
     target_spc.save_image(difference, op.join(outdir, "difference.nii.gz"))
     logging.info(f"Difference image saved to {op.join(outdir, 'difference.nii.gz')}")
+
+    if zblur:
+        difference_zblur = truth - prediction_zblur
+        difference_zblur[prediction_zblur == 0] = 0
+        target_spc.save_image(difference_zblur, op.join(outdir, "difference_zblur.nii.gz"))
+        logging.info(f"Difference image with z-axis blurring saved to {op.join(outdir, 'difference_zblur.nii.gz')}")
+
     logging.info("Prediction done")
 
 
-def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
+def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0, outfolder=None, zblur=False):
     """Calculates statistics for the predicted perfusion images in the given space.
 
     Parameters
@@ -287,9 +337,12 @@ def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
         The probability threshold for the ROI masks.
     """
 
+    if outfolder is None:
+        outfolder = op.basename(modeldir).split(".")[0]
+
     if space == "all":
         # find all spaces inside the ppr directory
-        spaces = os.listdir(op.join(oxdir, "ppr", op.basename(modeldir).split(".")[0]))
+        spaces = os.listdir(op.join(oxdir, "ppr", outfolder))
         # remove anything other than native, struct, std
         spaces = [s for s in spaces if s in ["native", "struct", "std"]]
     else:
@@ -301,7 +354,7 @@ def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
 
     for space in spaces:
         logging.info(f"Calculating ROI statistics for space {space}")
-        outdir = op.join(oxdir, "ppr", op.basename(modeldir).split(".")[0], space)
+        outdir = op.join(oxdir, "ppr", outfolder, space)
         logging.basicConfig(
             filename=op.join(outdir, "ppr.log"),
             level=LOGGING_LEVEL,
@@ -331,6 +384,9 @@ def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
         truth = nib.load(op.join(outdir, "truth.nii.gz")).get_fdata()
         prediction = nib.load(op.join(outdir, "prediction.nii.gz")).get_fdata()
         difference = nib.load(op.join(outdir, "difference.nii.gz")).get_fdata()
+        if zblur:
+            prediction_zblur = nib.load(op.join(outdir, "prediction_zblur.nii.gz")).get_fdata()
+            difference_zblur = nib.load(op.join(outdir, "difference_zblur.nii.gz")).get_fdata()
         roi_masks = model_dict["roi_masks"]
 
         if (space == "native") or (space == "struct"):
@@ -349,6 +405,8 @@ def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
                 std2asl = rt.chain(std2struct, struct2asl)
 
         roi_list = []
+        if zblur:
+            roi_list_zblur = []
 
         for i, roi_key in enumerate(model_dict["roi_dict"]):
             logging.debug(f"Calculating statistics for ROI {roi_key}")
@@ -371,6 +429,9 @@ def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
             roi_truth = truth[roi_mask]
             roi_prediction = prediction[roi_mask]
             roi_difference = difference[roi_mask]
+            if zblur:
+                roi_prediction_zblur = prediction_zblur[roi_mask]
+                roi_difference_zblur = difference_zblur[roi_mask]
             roi_stats = {
                 "roi_short": roi_key,
                 "roi_full": model_dict["roi_dict"][roi_key],
@@ -383,8 +444,23 @@ def roi_stats(oxdir, modeldir, space="all", roiprobthr=50.0):
                 "rmse_difference": np.sqrt(np.mean(roi_difference ** 2)),
             }
             roi_list.append(roi_stats)
+            if zblur:
+                roi_stats_zblur = {
+                    "roi_short": roi_key,
+                    "roi_full": model_dict["roi_dict"][roi_key],
+                    "mean_truth_zblur": roi_truth.mean(),
+                    "std_truth_zblur": roi_truth.std(),
+                    "mean_prediction_zblur": roi_prediction_zblur.mean(),
+                    "std_prediction_zblur": roi_prediction_zblur.std(),
+                    "me_difference_zblur": roi_difference_zblur.mean(),
+                    "mae_difference_zblur": np.abs(roi_difference_zblur).mean(),
+                    "rmse_difference_zblur": np.sqrt(np.mean(roi_difference_zblur ** 2)),
+                }
+                roi_list_zblur.append(roi_stats_zblur)
         
         pd.DataFrame(roi_list).to_csv(op.join(outdir, "roi_stats.csv"), index=False)
+        if zblur:
+            pd.DataFrame(roi_list_zblur).to_csv(op.join(outdir, "roi_stats_zblur.csv"), index=False)
         logging.info(f"ROI statistics saved to {op.join(outdir, 'roi_stats.csv')}")
 
     logging.info("ROI statistics done")
